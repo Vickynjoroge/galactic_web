e98111dd23aa353c9d7542602d3562b6e1fd8b24
Victor Ngigi Njoroge <vnjorogen2001@gmail.com>
2023-05-02 21:11:44 +0300

    loads bot_card

--- src/component/BotCollection.js
+++ src/component/BotCollection.js
@@ -2,19 +2,15 @@ import React from 'react'
 import '../componentCSS/BotCollection.css'
 
 function BotCollection({name, health, damage, armor, bot_class, catchphrase, avatar_url, created_at, updated_at }) {
-    function handleClick(){
-        handleAddBotToArmy({name, health, damage, armor, bot_class, catchphrase, avatar_url, created_at, updated_at })
-
-    }
   return (
-    <div className='botCard' onClick={handleClick}>
+    <div className='botCard'>
         <img src={avatar_url} alt='bot image' />
         <h2>{name}</h2>
         <p>Health: {health}</p>
         <p>Damage: {damage}</p>
         <p>Armor: {armor}</p>
         <p>Bot_Class: {bot_class}</p>
-        <p>Catch Phrase: {catchphrase}</p>
+        <p style={{color:'#617d98', fontSize: '0.75rem',marginTop:'0.25rem' }}>Catch Phrase: {catchphrase}</p>
         <p>Created at: {created_at}</p>
         <p>Updated at: {updated_at}</p>
     </div>
