e7003c21e29e3ec35fbad6e1ccdafcaa502c26f3
Victor Ngigi Njoroge <vnjorogen2001@gmail.com>
2023-05-02 21:17:29 +0300

    holds data for the bot_card that loads to bot_Army

---
+++ src/component/SelectedBot.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function SelectedBot({bot}) {
+    const {name, health, damage, armor, bot_class, catchphrase, avatar_url, created_at, updated_at } = bot
+  return (
+    <div>
+        <div>
+            <h2>{name}</h2>
+            <img src={avatar_url} alt='bot avatar' />
+            <p>Health: {health}</p>
+            <p>Damage: {damage}</p>
+            <p>Armor: {armor}</p>
+            <p>Bot_Class: {bot_class}</p>
+            <p style={{color:'#617d98', fontSize: '0.75rem',marginTop:'0.25rem' }}>Catch Phrase: {catchphrase}</p>
+            <p>Created at: {created_at}</p>
+            <p>Updated at: {updated_at}</p>
+        </div>
+    </div>
+  )
+}
+
+export default SelectedBot
